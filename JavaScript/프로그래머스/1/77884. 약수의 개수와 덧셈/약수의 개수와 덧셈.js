1b80a377ce4d88cb7e29dd6cf5932a731562a001
서지유 <164027964+SJY08@users.noreply.github.com>
2024-08-29 08:09:26 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 32.87 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,10 @@
+function solution(left, right) {
+    let answer = 0;
+    for(let i = left ; i <= right ; i++){
+        let a = 0
+        for(let j = 1 ; j <= i ; j++) if(i%j==0) a++
+        if(a%2==0) answer += i
+        else answer -= i
+    }
+    return answer;
+}
