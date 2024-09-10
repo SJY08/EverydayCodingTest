c536575a75028455c6b8346d60aba85c94d306b8
서지유 <164027964+SJY08@users.noreply.github.com>
2024-09-10 14:37:29 +0900

    [level 0] Title: 카운트 다운, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ JavaScript/프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,5 +1,1 @@
-function solution(start_num, end_num) {
-    let answer = []
-    for(let i = start_num ; i >= end_num ; i--) answer.push(i)
-    return answer
-}
+let solution = (start_num, end_num) => Array(start_num-end_num+1).fill(start_num).map((v,i) => v-i)
