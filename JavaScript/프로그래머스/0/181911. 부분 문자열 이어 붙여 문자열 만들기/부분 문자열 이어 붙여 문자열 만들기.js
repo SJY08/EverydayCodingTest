a44af5523b3a17c45c598f805b45ed3a73e782dc
서지유 <164027964+SJY08@users.noreply.github.com>
2024-09-20 08:51:12 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ JavaScript/프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,1 +1,13 @@
+/*
 let solution = (my_strings, parts) => my_strings.map((v,i) => v.slice(parts[i][0],parts[i][1]+1)).join("")
+*/
+
+function solution(my_strings, part){
+    let answer = ''
+    
+    for(let i = 0 ; i < my_strings.length ; i++){
+        answer+= my_strings[i].slice(part[i][0],part[i][1]+1)
+    }
+    
+    return answer
+}
