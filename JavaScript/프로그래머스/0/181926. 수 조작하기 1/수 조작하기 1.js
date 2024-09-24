53e30622feb222ecd19a047f04425e5e21ee6e49
서지유 <164027964+SJY08@users.noreply.github.com>
2024-09-24 14:57:07 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.97 ms, Memory: 38.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ JavaScript/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,1 +1,13 @@
-let solution = (n,control) => n+[...control].map(v=>v=="w"?1:v=="s"?-1:v=="d"?10:-10).reduce((a,b)=>a+b,0)
+//let solution = (n,control) => n+[...control].map(v=>v=="w"?1:v=="s"?-1:v=="d"?10:-10).reduce((a,b)=>a+b,0)
+
+function solution(n,control){
+    for(let v of control){
+        switch(v){
+            case "w": n+=1; break
+            case "s": n-=1; break
+            case "d": n+=10; break
+            case "a": n-=10; 
+        }
+    }
+    return n
+}
