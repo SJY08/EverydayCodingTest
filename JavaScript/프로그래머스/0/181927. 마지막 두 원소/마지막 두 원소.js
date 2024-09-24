4429524f067ed1f92f59b4148096233493c10223
서지유 <164027964+SJY08@users.noreply.github.com>
2024-09-24 15:02:14 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,6 +1,8 @@
-function solution(num_list) {
-    const last = num_list[num_list.length-1]
-    const secondLast = num_list[num_list.length-2]
-    last>secondLast? num_list.push(last-secondLast) : num_list.push(last*2)
-    return num_list
-}
+// function solution(num_list) {
+//     const last = num_list[num_list.length-1]
+//     const secondLast = num_list[num_list.length-2]
+//     last>secondLast? num_list.push(last-secondLast) : num_list.push(last*2)
+//     return num_list
+// }
+
+let solution=num_list=>num_list[num_list.length-1]>num_list[num_list.length-2]?[...num_list,num_list[num_list.length-1]-num_list[num_list.length-2]]:[...num_list,num_list[num_list.length-1]*2]
