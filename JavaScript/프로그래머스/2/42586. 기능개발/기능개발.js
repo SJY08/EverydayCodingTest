8596419b7cacf96bb876b50ec98e3a6958f6bca0
서지유 <164027964+SJY08@users.noreply.github.com>
2024-09-25 16:55:10 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42586. 기능개발/기능개발.js
+++ JavaScript/프로그래머스/2/42586. 기능개발/기능개발.js
@@ -3,15 +3,18 @@ return 각 배포마다 몇개의 기능이 배포되는지에 대한 배열
 
 1. 각 배포 별 걸리는 일 수 배열 선언 (leftDay)
 2. leftDay의 첫번째 값을 변수에 저장(progress), 걸리는 일 수(day = 1) 변수 선언
+
 3. 배포 반복(i = 1부터)
-4. 만약 progress보다 left[i] 값이 작거나 같다면
-    - day 값 증가
-5. 아니면 ?
-    - answer 배열에 day 푸시
-    - progress = leftDay[i]
-    - day = 1
-6. 만약 i가 마지막 인덱스라면?
-    - answer 배열에 day 푸시
+    4. 만약 progress보다 left[i] 값이 작거나 같다면
+        - day 값 증가
+    5. 아니면 ?
+        - answer 배열에 day 푸시
+        - progress = leftDay[i]
+        - day = 1
+    6. 만약 i가 마지막 인덱스라면?
+        - answer 배열에 day 푸시
+
+7. answer 반환
 */
 
 function solution(progresses, speeds) {
