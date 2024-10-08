174b4bd7ad614d96e0efb06deec40aa736b107b5
서지유 <164027964+SJY08@users.noreply.github.com>
2024-10-08 14:49:28 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ JavaScript/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,1 +1,16 @@
-let solution = (str_list, ex) => str_list.filter(v=>!v.includes(ex)).join("")
+//let solution = (str_list, ex) => str_list.filter(v=>!v.includes(ex)).join("")
+
+function solution(str_list,ex){
+    let answer = ""
+    for(let v of str_list){
+        let a = true
+        for(let i = 0 ; i < v.length ; i++){
+            if(v.slice(i,i+ex.length)==ex){
+                a=false
+                break
+            }
+        }
+        if(a) answer+=v
+    }
+    return answer
+}
