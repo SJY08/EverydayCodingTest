3e1a71d17391bb0463476315244a9623db8731a7
서지유 <164027964+SJY08@users.noreply.github.com>
2024-10-16 16:52:09 +0900

    [level 2] Title: 구명보트, Time: 40.04 ms, Memory: 38 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42885. 구명보트/구명보트.js
+++ JavaScript/프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,3 +1,19 @@
+/*
+return 구출하기 위해 필요한 구명보트 최소 개수
+
+1. people 리스트를 정렬
+2. 왼쪽부터 시작할 인덱스 i와 오른쪽부터 시작할 인덱스 j 선언
+3. return 시킬 변수 선언
+
+4. i가 j와 만나기 전까지 반복
+    4-1. 만약 people의 인덱스 i인 값과 인덱스 j인 값의 합이 무게 제한(limit)의 이하라면
+        4-1-1. i의 값을 증가
+    4-2. j의 값 감소
+    4-3. count(구명보트의 개수) 증가
+
+5. 반복이 끝나면 count 반환
+*/
+
 function solution(people, limit) {
     people.sort((a,b)=>a-b)
     let i = 0
