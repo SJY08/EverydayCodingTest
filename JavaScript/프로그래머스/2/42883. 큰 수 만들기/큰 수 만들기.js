c0c7bbcb88063be677ba820a8c445c5edb3333b8
서지유 <164027964+SJY08@users.noreply.github.com>
2024-10-16 16:55:54 +0900

    [level 2] Title: 큰 수 만들기, Time: 39.55 ms, Memory: 49.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ JavaScript/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,3 +1,19 @@
+/*
+return 만들 수 있는 가장 큰 숫자(문자열)
+
+1. 리턴 시킬 배열 생성(answer)
+
+2. number의 길이만큼 반복
+    2-1. answer의 길이가 0 초과이고 answer의 마지막 값이 number[i] 미만이고 k가 0 초과일때 반복
+        2-1-1. k 감소
+        2-1-2. answer 마지막 값 삭제
+    2-2. answer에 number[i] 값 추가
+
+3. answer에서 number의 길이에서 k를 뺀 값부터 k만큼 삭제
+
+4. answer를 문자열 형태로 합친 후 반환
+*/
+
 function solution(number, k) {
     let answer = []
     
