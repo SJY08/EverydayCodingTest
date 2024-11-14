c99612421cc8ea20713a05b80d3bd4545d2e5d69
서지유 <164027964+SJY08@users.noreply.github.com>
2024-11-14 14:10:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,1 @@
-function solution(num) {
-    if(num == 1) return 0
-    let count = 0 ;
-    while(1){
-        if(count==500) return -1
-        if(num%2==0){
-            num/=2
-            count++
-        }
-        else{
-            num = num*3+1
-            count++
-        }
-        if(num==1) return count
-    }
-}
+const solution = (n,count=0) => n==1 ? ((count >= 500)?-1:count) : solution((n%2==0)?n/2:(n*3)+1, ++count)
