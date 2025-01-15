5483634d1ebfd1120fd6c2dd69e25dca211b87e3
서지유 <164027964+SJY08@users.noreply.github.com>
2025-01-15 20:30:31 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ Python/백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,19 @@
+import math
+
+T = int(input())
+
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = list(map(int,input().split()))
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    
+    if distance == 0 :
+        if r1 == r2 : print(-1)
+        else : print(0)
+            
+    else :
+        if r1+r2 == distance or abs(r2-r1) == distance:
+            print(1)
+        elif abs(r1-r2) < distance and distance < r1+r2:
+            print(2)
+        else:
+            print(0)
