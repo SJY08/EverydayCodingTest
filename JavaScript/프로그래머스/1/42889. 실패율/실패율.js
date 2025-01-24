59537504879a2c378f243c5f53fc4cfcef22e4b1
서지유 <164027964+SJY08@users.noreply.github.com>
2025-01-24 18:17:47 +0900

    [level 1] Title: 실패율, Time: 6102.34 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,12 @@
+function solution(N, stages) {
+    let answer = []
+    
+    for(let i = 1 ; i <= N ; i++) {
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        answer.push([i, curr/reach]);
+    }
+    
+    answer.sort((a,b) => b[1]-a[1])
+    return answer.map(v => v[0])
+}
