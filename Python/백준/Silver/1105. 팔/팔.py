f2f139492ae9c7695ba4c022f706465f69a9374d
서지유 <164027964+SJY08@users.noreply.github.com>
2025-01-27 18:33:09 +0900

    [Silver I] Title: 팔, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ Python/백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,15 @@
+L, R = map(str, input().split(' '))
+
+ret = 0
+
+if len(L) != len(R):
+    print(0)
+
+else:
+    for i in range(len(L)):
+        if L[i] == R[i]:
+            if L[i] == '8':
+                ret += 1
+        else:
+            break
+    print(ret)
