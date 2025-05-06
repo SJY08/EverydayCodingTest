cd8188127e765b0b43d824562524e8b451f8793b
서지유 <164027964+SJY08@users.noreply.github.com>
2025-05-07 08:29:02 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(n: Int, aa: Int, bb: Int): Int {
+        var a = aa
+        var b = bb
+        var turn =0
+        while(a!=b){
+            a = (a+1)/2
+            b = (b+1)/2
+            turn++
+        }
+        return turn
+    }
+}
