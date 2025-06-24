b5319606432660d7c8b07c198bf23edb15e5bc60
서지유 <164027964+SJY08@users.noreply.github.com>
2025-06-25 07:15:34 +0800

    [level 0] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ JavaScript/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,10 +1,7 @@
 function solution(numer1, denom1, numer2, denom2) {
     let den = lcm(denom1, denom2)
-    numer1 = numer1 * (den / denom1)
-    numer2 = numer2 * (den / denom2)
-    let numer3 = numer1 + numer2
+    let numer3 = numer1 * (den / denom1) + numer2 * (den / denom2)
     return [numer3 / gcd(numer3, den), den / gcd(numer3, den)]
 }
-
 const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b)
 const lcm = (a, b) => a * b / gcd(a, b)
