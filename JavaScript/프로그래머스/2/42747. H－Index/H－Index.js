35e4b757ebe656ec3cd7c408d0261eb385504dac
서지유 <164027964+SJY08@users.noreply.github.com>
2025-08-01 21:09:46 +0900

    [level 2] Title: H-Index, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42747. H－Index/H－Index.js
+++ JavaScript/프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,8 +1,6 @@
 function solution(citations) {
     let answer = 0
     citations.sort((a,b) => b-a)
-    for(let i = 1 ; i <= citations.length ; i++) {
-        answer = Math.max(answer, Math.min(i, citations[i-1]))
-    }
+    for(let i = 1 ; i <= citations.length ; i++) answer = Math.max(answer, Math.min(i, citations[i-1]))
     return answer
 }
