35abf536474cdf5ebcd22d40a5461e0fe8bc9c04
서지유 <164027964+SJY08@users.noreply.github.com>
2026-01-22 18:00:55 +0900

    [level 2] Title: 튜플, Time: 27.75 ms, Memory: 48 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/64065. 튜플/튜플.js
+++ JavaScript/프로그래머스/2/64065. 튜플/튜플.js
@@ -1,8 +1,1 @@
-function solution(s) {
-    let answer = []
-    const tuple = s.slice(1, s.length-1).split("}").map(v=> v.startsWith("{") ? v.slice(1).split(",").map(a=> +a) : v.startsWith(",{") ? v.slice(2).split(",").map(a=> +a) : undefined).sort((a,b) => a.length - b.length).filter(v=> v !== undefined)
-      
-    for(let v of tuple ) answer = [...new Set([ ...answer, ...v ])]
-    
-    return answer
-}
+const solution = s => s.slice(1, -1).split("}").map(v => v.startsWith("{") ? v.slice(1) : v.startsWith(",{") ? v.slice(2) : null).filter(Boolean).map(v => v.split(",").map(Number)).sort((a,b)=>a.length-b.length).reduce((a,v)=>[...new Set([...a,...v])],[])
