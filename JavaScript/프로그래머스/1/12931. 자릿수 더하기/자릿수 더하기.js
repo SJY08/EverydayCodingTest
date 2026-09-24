7856131a9575a1df64463bbc7fad75ea720d5917
서지유 <164027964+SJY08@users.noreply.github.com>
2026-09-24 14:01:57 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.09 ms, Memory: 43.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,1 +1,12 @@
-let solution = n => [...""+n].reduce((a,b)=>+a+(+b),0)
+function solution(n) {
+    // 문자열로 변환
+    let numbers = n.toString()
+    let answer = 0
+    
+    for(let num of numbers) {
+        answer += parseInt(num)
+    }
+    
+    return answer
+    
+}
