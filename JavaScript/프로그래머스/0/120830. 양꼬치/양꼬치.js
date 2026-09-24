b116cb4fc7571531ee37b64d125e3745ce11a8f8
서지유 <164027964+SJY08@users.noreply.github.com>
2026-09-24 13:55:12 +0900

    [level 0] Title: 양꼬치, Time: 0.07 ms, Memory: 43.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ JavaScript/프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,1 +1,7 @@
-let solution=(n,k)=>n*12000+(k-Math.floor(n/10))*2000
+function solution(n, k) {
+    let answer = 0
+    
+    let service_count = Math.floor(n/10)
+    
+    return n * 12000 + (k-service_count) * 2000
+}
