667bff2803786f1a6e089e5cc9e84afcb6a9e593
서지유 <164027964+SJY08@users.noreply.github.com>
2026-09-24 14:03:31 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.09 ms, Memory: 43.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ JavaScript/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,1 +1,6 @@
-let solution = n => [...""+n].reverse().map(a=>+a)
+function solution(n) {
+    // 문자열로 변환 후 spread 시켜서 배열 -> map함수로 숫자 배열로 변환
+    let number_arr = [...n.toString()].map(v => parseInt(v))
+    
+    return number_arr.reverse()
+}
